835aa21addb5c6674b957cff5af100063af23e1b
Ayorinde Adunse <adunseayorinde@gmail.com>
2020-07-24 01:18:37 +0100

    Get and delete post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -37,4 +37,62 @@ router.post(
   }
 );
 
+// @route  GET api/posts
+// @desc   Get all posts
+// @access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route  GET api/posts/:id
+// @desc   Get post by id
+// @access Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    // the line below exists to confirm if the id passed is a valid object id
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route  DELETE api/posts/:id
+// @desc   Delete a post
+// @access Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // confirm that the user deleting the post is the one authorised to do so
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    await post.remove();
+    return res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(400).send("Post not found");
+    }
+  }
+  res.status(500).send("Server Error");
+});
+
 module.exports = router;
