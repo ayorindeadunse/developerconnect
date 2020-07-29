087fca6865b9801af64a9c7e09a5523596525348
Ayorinde Adunse <adunseayorinde@gmail.com>
2020-07-29 10:42:39 +0100

    completed login module and nav and logout links

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -9,29 +9,46 @@ import Alert from "./components/layout/Alert";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 import "./App.css";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Alert />
-          <Switch>
-            <Route
-              exact
-              path='/register'
-              component='/register'
-              component={Register}
-            />
-            <Route exact path='/login' component='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  // this line of code(the useEffect react hook) should run once because for each state update,
+  // the useEffect react hook call here will be a constant loop of trying to authenticate the user,
+  // we don't want this to happen so it is called once and an empty array being passed as a
+  // second parameter in the method makes this happen.
+  // using this hook is a short cut from writing a component class with the lifecycle methods
+  // componentDidMount, componentDidUpdate(for updated state) and componentWillUnmount.
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route
+                exact
+                path='/register'
+                component='/register'
+                component={Register}
+              />
+              <Route exact path='/login' component='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
